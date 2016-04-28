a40f9be5f01ae38b2eacc009fb83c808631a52ae
Jim Pivarski <jpivarski@gmail.com>
2016-04-27 21:30:55 -0500

    programmatically updates

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ execfile("core.py")
 def sqr(x, y):
     return x**2 + y
 
+argtype = {"x": "double", "y": "int"}
+rettype = "double"
+
 fcndef = Rule("fcndef",
               FunctionDef(Any("name", str), Any("args"), Any("body"), []),
-              lambda args, body, name: Block(Format("double {0}({1:node}) {{", name, args), body, Format("}}")))
+              lambda args, body, name: Block(Format("{0} {1}({2:node}) {{", rettype, name, args), body, Format("}}")))
 
 param = Rule("param",
              Name(Any("name", str), Param()),
-             lambda name: Format("double {0}", name))
+             lambda name: Format("{0} {1}", argtype[name], name))
 
 ret = Rule("ret",
            Return(Any("s")),
@@ -25,9 +28,10 @@ powN = Rule("powN",
              lambda base, exponent: Format("pow({0:node}, {1:node})", base, exponent))
 
 result = Transpiler(sqr, [fcndef, param, ret, pow2, powN]).transform()
-
 print
 print "======================================================"
 print
 print result
 print
+print "======================================================"
+Transpiler(sqr, [fcndef, param, ret, pow2, powN]).python()
